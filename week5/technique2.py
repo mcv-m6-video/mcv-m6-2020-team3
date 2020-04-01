acac6b340442b904af6b5598ed79056a0096c58c
Marc Orós <marcoc619@gmail.com>
2020-04-01 18:18:30 +0200

    Skeleton files for w5

---
+++ week5/technique2.py
@@ -0,0 +1,154 @@
+"""
+example
+{'frame': 100, 'left': 931, 'top': 78, 'width': 82, 'height': 68, 'confidence': 0.99}
+Note, frame starts from 1.
+"""
+
+import pickle
+from utils import addBboxesToFrames, calculate_mAP, bb_iou, addBboxesToFrames_gif, upscaleDetections, adjustBboxWithOpticalFlow
+from utils_tracking import read_tracking_annotations, compute_mAP_track, addTracksToFrames, addTracksToFrames_gif
+from tqdm import tqdm
+from track import Track
+import opt_flow as of
+import glob
+import cv2
+import sys
+
+def detection_to_box(detection):
+    return [detection['left'], detection['top'], detection['left']+detection['width'],
+            detection['top']+detection['height']]
+
+def find_tracking(detections_all, video_length, missing_chance = 1, lou_max_threshold = 0.01, use_of=False, crop_center=False):
+    video_path = "./Datasets/AICity/frames/"
+    frameFiles = glob.glob(video_path + '/*.jpg')
+    frameFiles = sorted(frameFiles)
+
+    tracks_update = []
+    chance_left = []
+    tracks_end = []
+    track_num = 0
+    curr_frame = cv2.imread(frameFiles[0])
+    for i in tqdm(range(video_length)):
+        frame_id = i+1
+        detections_on_frame = [x for x in detections_all if x["frame"] == frame_id]
+        if frame_id == 1:
+            for detection in detections_on_frame:
+                track_one = Track(track_num, [detection])
+                track_num = track_num + 1
+                tracks_update.append(track_one)
+                chance_left.append(missing_chance)
+        else:
+            prev_frame = curr_frame
+            curr_frame = cv2.imread(frameFiles[frame_id-1])
+            if use_of is True:
+                frameOF = of.farneback(prev_frame, curr_frame)
+            # use end flag to record the update of old tracks
+            num_tracks_update = len(tracks_update)
+            # remember decrease the chance
+            for index in range(num_tracks_update):
+                chance_left[index] -= 1
+            for detection in detections_on_frame:
+                lou_max = 0.0
+                # here must loop end_flag instead of tracks_update, the latter will be updated
+                for index in range(num_tracks_update):
+                    # compare the detection and the last detection in tracks
+                    box1 = detection_to_box(detection)
+                    box2 = detection_to_box(tracks_update[index].detections[-1])
+                    if use_of is True:
+                        adjusted_box2 = adjustBboxWithOpticalFlow(box2, frameOF, crop_center)
+                    else:
+                        adjusted_box2 = box2
+                    lou = bb_iou(box1, adjusted_box2)
+                    if lou > lou_max:
+                        lou_max = lou
+                        index_max = index
+                if lou_max > lou_max_threshold:
+                    tracks_update[index_max].detections.append(detection)
+                    chance_left[index_max] = missing_chance
+                else:
+                    track_one = Track(track_num, [detection])
+                    track_num = track_num + 1
+                    tracks_update.append(track_one)
+                    chance_left.append(missing_chance)
+            del_index = []
+            for index, chance in enumerate(chance_left):
+                if chance <= 0:
+                    del_index.append(index)
+            for index in reversed(del_index):
+                tracks_end.append(tracks_update[index])
+                tracks_update.pop(index)
+                chance_left.pop(index)
+
+    tracks_end.extend(tracks_update)
+
+    return tracks_end
+
+if __name__ == "__main__":
+    #detections_filename = "./detections/results_t12_first.pkl"
+    detections_filename = "./detections/results_t12_interleaved.pkl"
+    use_of = False if len(sys.argv) > 1 and sys.argv[1] == "-n" else True
+    crop_center = True if len(sys.argv) > 1 and sys.argv[1] == "-c" else False
+    
+    video_length = 2141
+    #video_length = 500
+    video_path = "./Datasets/AICity/frames/"
+    groundtruth_xml_path = 'Datasets/AICity/aicity_annotations.xml'
+
+    print("Reading pkl")
+    with open(detections_filename, 'rb') as p:
+        detections = pickle.load(p)
+        p.close()
+
+    detections = upscaleDetections(detections)
+
+    print("Reading annotations...")
+    read_annotations_flag = False
+    annotations_pkl_filename = "gt_annotations.pkl"
+    if read_annotations_flag:
+        # groundTruth = read_annotations(groundtruth_xml_path, video_length)
+        groundTruth, tracks_gt_list = read_tracking_annotations(groundtruth_xml_path, video_length)
+        with open(annotations_pkl_filename, 'wb') as f:
+            pickle.dump([groundTruth, tracks_gt_list], f)
+            f.close()
+    else:
+        print("Reading pkl")
+        with open(annotations_pkl_filename, 'rb') as p:
+            groundTruth, tracks_gt_list = pickle.load(p)
+            p.close()
+
+    #addBboxesToFrames_gif(video_path, detections, groundTruth, start_frame=210, end_frame=260, name="test")
+
+    # sort detections because detections is sort by confidence while calculating map.
+    detections.sort(key=lambda x: x['frame'])
+
+    # detections_tracks = find_tracking(detections, video_length, missing_chance=1, lou_max_threshold=0.01)
+    missing_chance_list = [1, 3, 5, 7]
+    lou_max_threshold_list = [0.01, 0.1, 0.3, 0.5, 0.7]
+    missing_chance_list = [3, 5, 7]
+    lou_max_threshold_list = [0.3, 0.5, 0.7]
+    result_list = []
+    for missing_chance in missing_chance_list:
+        for lou_max_threshold in lou_max_threshold_list:
+            detections_tracks = find_tracking(detections, video_length, missing_chance=missing_chance,
+                                              lou_max_threshold=lou_max_threshold, use_of=use_of, crop_center=crop_center)
+            with open("track_gt_de.pkl", 'wb') as f:
+                pickle.dump([tracks_gt_list, detections_tracks, video_length], f)
+                f.close()
+            mAP_track = compute_mAP_track(tracks_gt_list, detections_tracks, IoU_threshold=0.5)
+            print("Missing chance: " + str(missing_chance))
+            print("IoU max thr: " + str(lou_max_threshold))
+            print("mAP_track = ", mAP_track)
+            result_list.append([missing_chance, lou_max_threshold, mAP_track])
+    print(result_list)
+
+    for track_one in detections_tracks:
+        track_one.detections.sort(key=lambda x: x['frame'])
+
+    #Plot next frame and previous frame adjusted with of
+    exit()
+
+    addTracksToFrames_gif(video_path, detections_tracks, tracks_gt_list, start_frame=210, end_frame=390, name="test")
+
+
+
+
---
+++ week5/utils_tracking.py
@@ -0,0 +1,232 @@
+import xml.etree.ElementTree as ET
+from track import Track
+from tqdm import tqdm
+import numpy as np
+from utils_w4 import calculate_mAP
+import glob
+import cv2
+import imageio
+import motmetrics as mm
+import pickle
+
+
+def detection_is_dictionary(frame_id, left, top, width, height, confidence = 1.0):
+    detection = {}
+    detection['frame'] = frame_id
+    detection['left'] = left
+    detection['top'] = top
+    detection['width'] = width
+    detection['height'] = height
+    detection['confidence'] = confidence
+    return detection
+
+def read_tracking_annotations(annotation_path, video_length):
+    """
+    Arguments:
+    capture: frames from video, opened as cv2.VideoCapture
+    root: parsed xml annotations as ET.parse(annotation_path).getroot()
+    """
+    root = ET.parse(annotation_path).getroot()
+
+    ground_truths = []
+    tracks = []
+    images = []
+    num = 0
+
+    for num in tqdm(range(video_length)):
+        #for now: (take only numannotated annotated frames)
+        #if num > numannotated:
+        #    break
+
+        for track in root.findall('track'):
+            gt_id = track.attrib['id']
+            label = track.attrib['label']
+            box = track.find("box[@frame='{0}']".format(str(num)))
+
+            #if box is not None and (label == 'car' or label == 'bike'):    # Read cars and bikes
+            if box is not None and label == 'car':                          # Read cars
+
+                #if box.attrib['occluded'] == '1':                           # Discard occluded
+                #    continue
+
+                #if label == 'car' and box[0].text == 'true':               # Discard parked cars
+                #    continue
+
+                frame = int(box.attrib['frame'])
+                #if frame < 534:
+                #    continue
+
+                xtl = int(float(box.attrib['xtl']))
+                ytl = int(float(box.attrib['ytl']))
+                xbr = int(float(box.attrib['xbr']))
+                ybr = int(float(box.attrib['ybr']))
+                ground_truths.append(detection_is_dictionary(frame, xtl, ytl, xbr - xtl + 1, ybr - ytl + 1, 1))
+                track_corresponding = [t for t in tracks if t.id == gt_id]
+                if len(track_corresponding) > 0:
+                    track_corresponding[0].detections.append(detection_is_dictionary(frame+1, xtl, ytl, xbr - xtl + 1, ybr - ytl + 1, 1))
+                else:
+                    track_corresponding = Track(gt_id, [detection_is_dictionary(frame+1, xtl, ytl, xbr - xtl + 1, ybr - ytl + 1, 1)])
+                    tracks.append(track_corresponding)
+
+    # print(ground_truths)
+    return ground_truths, tracks
+
+
+def compute_mAP_track(groundtruth_tracks, detections_tracks, IoU_threshold=0.5, verbose = False):
+    """
+    Calculate the mean of the best mAP for each track of the ground truth
+    input:
+    groundtruth_tracks: tracks of gt, the format should be list of Class Track
+    detections_tracks: tracks of detections, the format should be list of Class Track
+    """
+
+    mAP_list = list()
+
+    for groundtruth_track in tqdm(groundtruth_tracks):
+        #print('DETECTION'), print(detection_track)
+        max_mAP = 0
+
+        for detection_track in detections_tracks:
+            #print(groundtruth_track)
+            mAP = calculate_mAP(groundtruth_track.detections, detection_track.detections,
+                                          IoU_threshold)
+            #print(mAP)
+            if(max_mAP < mAP):
+                max_mAP = mAP
+
+        mAP_list.append(max_mAP)
+
+    np_mAP_list = np.asarray(mAP_list)
+
+    mAP_mean = np.mean(np_mAP_list)
+    if verbose:
+        print("np_mAP_list:")
+        print(np_mAP_list)
+        print("mAP = {}".format(mAP_mean))
+
+    return mAP_mean
+
+
+def center_of_detection(detection):
+    return (int(detection['left'] + 0.5 * detection['width']), int(detection['top'] + 0.5 * detection['height']))
+
+
+def write_one_frame(detections_tracks, frame_id, frameMat, color):
+    """
+    tool for addTracksToFrames
+    :param detections_tracks: this can be detections or ground truth
+    :param frame_id: which frame
+    :param frameMat: frame picture
+    :param color: rectangle and line color
+    :return: frameMat
+    """
+    for track_one in detections_tracks:
+        index = 0
+        flag_shoot = False
+        for index, detection in enumerate(track_one.detections):
+            # write the rectangle
+            if detection['frame'] == frame_id:
+                startPoint = (int(detection['left']), int(detection['top']))
+                endPoint = (int(startPoint[0] + detection['width']), int(startPoint[1] + detection['height']))
+                frameMat = cv2.rectangle(frameMat, startPoint, endPoint, color, 2)
+                flag_shoot = True
+                break
+        if flag_shoot:
+            shoot_index = index
+            # write the line
+            for index in range(shoot_index - 1):
+                startPoint = center_of_detection(track_one.detections[index])
+                endPoint = center_of_detection(track_one.detections[index + 1])
+                frameMat = cv2.line(frameMat, startPoint, endPoint, color, 2)
+    return frameMat
+
+
+def addTracksToFrames(framesPath, detections_tracks, tracks_gt_list, start_frame = 1, end_frame = 2141, name = "test"):
+    """
+    write the video of the tracking result
+    :param framesPath: path of frames
+    :param detections_tracks: detections in list of Track. Detections in Track should be sorted.
+    :param tracks_gt_list: ground truth in list of Track. Detections in Track should be sorted.
+    :param name: name of video.
+    :return: None
+    """
+    size = (1920, 1080)
+    fps = 10
+    out = cv2.VideoWriter(name + '.avi', cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+
+    for frame_id in tqdm(range(start_frame, end_frame)):
+        filename = "{}{}.jpg".format(framesPath, str(frame_id).zfill(5))
+        frameMat = cv2.imread(filename)
+        color_detection = (0, 0, 255)
+        write_one_frame(detections_tracks, frame_id, frameMat, color_detection)
+        color_gt = (255, 0, 0)
+        write_one_frame(tracks_gt_list, frame_id, frameMat, color_gt)
+        out.write(frameMat)
+    out.release()
+
+
+def addTracksToFrames_gif(framesPath, detections_tracks, tracks_gt_list, start_frame = 1, end_frame = 10, name = "test"):
+    """
+    write the gif of the tracking result
+    :param framesPath: path of frames
+    :param detections_tracks: detections in list of Track. Detections in Track should be sorted.
+    :param tracks_gt_list: ground truth in list of Track. Detections in Track should be sorted.
+    :param name: name of video.
+    :return: None
+    """
+    images = []
+
+    for frame_id in tqdm(range(start_frame, end_frame)):
+        filename = "{}{}.jpg".format(framesPath, str(frame_id).zfill(5))
+        frameMat = cv2.imread(filename)
+        color_detection = (0, 0, 255)
+        write_one_frame(detections_tracks, frame_id, frameMat, color_detection)
+        color_gt = (255, 0, 0)
+        write_one_frame(tracks_gt_list, frame_id, frameMat, color_gt)
+        resized = cv2.resize(frameMat, (480, 270), interpolation=cv2.INTER_AREA)
+        images.append(resized)
+    imageio.mimsave(name + '.gif', images)
+
+
+
+
+def find_frame_in_track(tracks, frame_id):
+    object_id_list = []
+    box_list = []
+    for track_one in tracks:
+        for index, detection in enumerate(track_one.detections):
+            # write the rectangle
+            if detection['frame'] == frame_id:
+                object_id_list.append(track_one.id)
+                box_list.append([detection['left'], detection['top'], detection['width'], detection['height']])
+                break
+    return object_id_list, box_list
+
+
+def compute_idf1(groundtruth_tracks, detections_tracks, video_length, IoU_threshold=0.5, verbose = False):
+    acc = mm.MOTAccumulator(auto_id=True)
+
+    for i in range(video_length):
+        frame_id = i + 1
+        gt_ids, gt_bboxes = find_frame_in_track(groundtruth_tracks, frame_id)
+        detections_ids, detections_bboxes = find_frame_in_track(detections_tracks, frame_id)
+
+        distances_gt_det = mm.distances.iou_matrix(gt_bboxes, detections_bboxes, max_iou=1.)
+        acc.update(gt_ids, detections_ids, distances_gt_det)
+
+    print(acc.mot_events)
+    mh = mm.metrics.create()
+    summary = mh.compute(acc, metrics=mm.metrics.motchallenge_metrics, name='acc')
+    print(summary)
+
+
+
+
+
+if __name__ == "__main__":
+    with open("track_gt_de.pkl", 'rb') as p:
+        tracks_gt_list, detections_tracks, video_length = pickle.load(p)
+        p.close()
+
+    compute_idf1(tracks_gt_list, detections_tracks, video_length)
+
