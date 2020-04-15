c1337314a8d664a5c0fb70c2a433ecf1121d9acc
Marc Orós <marcoc619@gmail.com>
2020-04-15 23:40:40 +0200

    MTMC and changes to track

--- week5/AICityIterator.py
+++ week5/AICityIterator.py
@@ -5,7 +5,6 @@ class AICityIterator:
     datasetStructure = {
         'S01': ['c001', 'c002', 'c003', 'c004', 'c005'],
         'S03': ['c010', 'c011', 'c012', 'c013', 'c014', 'c015'],
-        # 'S03': ['c010','c012'],
         'S04': ['c016', 'c018', 'c020', 'c022', 'c024', 'c026', 'c028', 'c030', 'c032', 'c034', 'c036', 'c038', 'c040','c017', 'c019', 'c021', 'c023', 'c025', 'c027', 'c029', 'c031', 'c033', 'c035', 'c037', 'c039']
     }
 
@@ -28,8 +27,6 @@ class AICityIterator:
         self.frameFiles = sorted(self.frameFiles)
         self.limit = videoLength if videoLength is not None else len(self.frameFiles)
         self.currItem = 0
-        # self.limit =318
-        # self.currItem = 218
 
     def __iter__(self):
         return self
@@ -42,13 +39,15 @@ class AICityIterator:
         raise StopIteration
 
     def __len__(self):
-        return len(self.frameFiles)  
+        return len(self.frameFiles)
+
+    def toList(self):
+        return self.frameFiles
 
 def getStructure():
     datasetStructure = {
         'S01': ['c001', 'c002', 'c003', 'c004', 'c005'],
         'S03': ['c010', 'c011', 'c012', 'c013', 'c014', 'c015'],
-        # 'S03': ['c010', 'c012'],
         'S04': ['c016', 'c018', 'c020', 'c022', 'c024', 'c026', 'c028', 'c030', 'c032', 'c034', 'c036', 'c038', 'c040','c017', 'c019', 'c021', 'c023', 'c025', 'c027', 'c029', 'c031', 'c033', 'c035', 'c037', 'c039']
     }
     return datasetStructure
